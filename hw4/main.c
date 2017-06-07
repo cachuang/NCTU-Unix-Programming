fef6b6c0e136f252691f2f5efb915be52b394b0a
Kevin <Kevin@Kevin-MacBook-Pro.local>
2017-06-08 02:32:12 +0800

    finish game logic

--- hw4/main.c
+++ hw4/main.c
@@ -85,16 +85,137 @@ int server_mode(int port)
 
 void change_turn()
 {
+    char message[MAXLINE];
+
     myturn = !myturn;
 
-    if (myturn)
-        draw_message("It's your turn...      ", 0);
-    else
-        draw_message("Waiting for peer...      ", 1);
-
-    refresh();
+    if (myturn) {
+        snprintf(message, sizeof(message), "Player #%d %s: It's your turn      ", player == PLAYER1 ? 1 : 2,
+                 player == PLAYER1 ? "(O)" : "(X)");
+        draw_message(message, 0);
+    }
+    else {
+        snprintf(message, sizeof(message), "Player #%d %s: Waiting for peer ...      ", player == PLAYER1 ? 1 : 2,
+                 player == PLAYER1 ? "(O)" : "(X)");
+        draw_message(message, 1);
+    }
 }
 
+bool update_board(int _x, int _y, int replace)
+{
+    int x, y;
+    bool updated = false;
+
+    // check top
+    x = _x; y = _y;
+    while(--y >= 0) {
+        if (board[y][x] == 0)
+            break;
+         if (board[y][x] == replace) {
+             for (y = y+1; y < _y; y++) {
+                 board[y][x] = replace;
+                 updated = true;
+             }
+             break;
+         }
+    }
+    // check bottom
+    x = _x; y = _y;
+    while(++y < BOARDSZ) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (y = y-1; y > _y; y--) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check left
+    x = _x; y = _y;
+    while(--x >= 0) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x+1; x < _x; x++) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check right
+    x = _x; y = _y;
+    while(++x < BOARDSZ) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x-1; x > _x; x--) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check top left
+    x = _x; y = _y;
+    while(--x >= 0 && --y >= 0) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x+1, y = y+1; (x < _x && y < _y); x++, y++) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check top right
+    x = _x; y = _y;
+    while(++x < BOARDSZ && --y >= 0) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x-1, y = y+1; (x > _x && y < _y); x--, y++) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check bottom left
+    x = _x; y = _y;
+    while(--x >= 0 && ++y < BOARDSZ) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x + 1, y = y - 1; (x < _x && y > _y); x++, y--) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+    // check bottom right
+    x = _x; y = _y;
+    while(++x < BOARDSZ && ++y < BOARDSZ) {
+        if (board[y][x] == 0)
+            break;
+        if (board[y][x] == replace) {
+            for (x = x-1, y = y-1; (x > _x && y > _y); x--, y--) {
+                board[y][x] = replace;
+                updated = true;
+            }
+            break;
+        }
+    }
+
+    if (updated)
+        draw_board();
+
+    return updated;
+}
 
 void game_start()
 {
@@ -113,7 +234,6 @@ void game_start()
 
     init_colors();
 
-restart:
     clear();
     cx = cy = 3;
     init_board();
@@ -130,60 +250,73 @@ restart:
 
     FD_ZERO(&rset);
 
-    if(myturn) {
-        draw_message("It's your turn...    ", 0);
-        refresh();
+    if (myturn) {
+        snprintf(message, sizeof(message), "Player #%d %s: It's your turn      ", player == PLAYER1 ? 1 : 2,
+                 player == PLAYER1 ? "(O)" : "(X)");
+        draw_message(message, 0);
     }
     else {
-        draw_message("Waiting for peer...    ", 1);
-        refresh();
+        snprintf(message, sizeof(message), "Player #%d %s: Waiting for peer ...      ", player == PLAYER1 ? 1 : 2,
+                 player == PLAYER1 ? "(O)" : "(X)");
+        draw_message(message, 1);
     }
+    refresh();
 
-    while(true) // main loop
+    while (true) // main loop
     {
         FD_SET(STDIN_FILENO, &rset);
         FD_SET(sockfd, &rset);
 
         select(sockfd+1, &rset, NULL, NULL, NULL);
 
-        if(FD_ISSET(sockfd, &rset))
+        if (FD_ISSET(sockfd, &rset))
         {
             int n = read(sockfd, message, MAXLINE);
-            if(n == 0) {
+
+            if (n == 0)
+            {
                 endwin();
                 printf("Peer has closed the connection.\n");
                 exit(0);
             }
             message[n] = 0;
 
-            if (!strncmp(message, "put", 3)) {
+            if (!strncmp(message, "put", 3))
+            {
                 int x, y;
+
                 sscanf(message, "%*s %d %d", &x, &y);
                 board[y][x] = opponent;
                 draw_cursor(x, y, 0);
                 draw_score();
-
+                update_board(x, y, opponent);
                 change_turn();
+                refresh();
             }
         }
-        if(FD_ISSET(0, &rset))
+        if (FD_ISSET(0, &rset))
         {
             int ch = getch();
             int moved = 0;
 
-            switch(ch) {
+            switch (ch)
+            {
                 case ' ':
                 case 0x0d:
                 case 0x0a:
                 case KEY_ENTER:
-                    if(myturn) {
-                        if(board[cy][cx] != 0)
+                    if (myturn)
+                    {
+                        // prevent from overwriting existing piece
+                        if(board[cy][cx] != 0 || !update_board(cx, cy, player))
                             break;
+
                         board[cy][cx] = player;
                         draw_cursor(cx, cy, 1);
                         draw_score();
-
+                        //update_board(cx, cy, player);
                         change_turn();
+                        refresh();
 
                         snprintf(message, sizeof(message), "put %d %d", cx, cy);
                         write(sockfd, message, sizeof(message));
@@ -192,10 +325,6 @@ restart:
                 case 'q':
                 case 'Q':
                     goto quit;
-                    break;
-                case 'r':
-                case 'R':
-                    goto restart;
                     break;
                 case 'k':
                 case KEY_UP:
@@ -227,7 +356,7 @@ restart:
                     break;
             }
 
-            if(moved) {
+            if (moved) {
                 refresh();
                 moved = 0;
             }
@@ -237,26 +366,31 @@ restart:
     }
 
 quit:
-    endwin();			// end curses mode
+    endwin();			    // end curses mode
     close(sockfd);
     exit(0);
 }
 
 int main(int argc, char **argv)
 {
-    if((argc < 2) || (strcmp(argv[1], "-c") && strcmp(argv[1], "-s"))) {
+    if ((argc < 2) || (strcmp(argv[1], "-c") && strcmp(argv[1], "-s")))
+    {
         printf("Usage: ./othello -s|-c\n");
         exit(0);
     }
-    else if(!strcmp(argv[1], "-s")) {
-        if(argc < 3) {
+    else if (!strcmp(argv[1], "-s"))
+    {
+        if(argc < 3)
+        {
             printf("Usage: ./othello -s <port>\n");
             exit(0);
         }
         sockfd = server_mode(atoi(argv[2]));
     }
-    else if(!strcmp(argv[1], "-c")) {
-        if(argc < 4) {
+    else if (!strcmp(argv[1], "-c"))
+    {
+        if(argc < 4)
+        {
             printf("Usage: ./othello -c <ip_address> <port>\n");
             exit(0);
         }
